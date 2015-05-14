8d79f4069956ef6933b1cdedad58384ba10f2f8b
dazzilove <dazzilove@gmail.com>
2015-05-14 12:51:34 +0900

    3장 완료

--- tobyspringstudy03/src/main/java/com/dazzilove/tobyspringstudy/user/dao/UserDao.java
+++ tobyspringstudy03/src/main/java/com/dazzilove/tobyspringstudy/user/dao/UserDao.java
@@ -1,28 +1,31 @@
 package com.dazzilove.tobyspringstudy.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.dazzilove.tobyspringstudy.user.domain.User;
 
 public class UserDao {
-	private DataSource dataSource;	
 	private JdbcTemplate jdbcTemplate;
+	
+	RowMapper<User> userMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
 	
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
@@ -31,40 +34,8 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		this.jdbcTemplate.queryForObject("select * from users where id = ?", 
-				new Object[] {id}, 
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-					
-				});
-		
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		
-		ResultSet rs = ps.executeQuery();
-		User user = null;
-		if(rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if(user == null) throw new EmptyResultDataAccessException(); 
-		
-		return user;
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
+				new Object[] {id},  userMapper);
 	}
 	
 	public void deleteAll() throws SQLException {
@@ -73,5 +44,9 @@ public class UserDao {
 	
 	public int getCount() throws SQLException {
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	}
+
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", userMapper);
 	}
 }
--- tobyspringstudy03/src/main/java/com/dazzilove/tobyspringstudy/user/dao/UserDaoSpringTest.java
+++ tobyspringstudy03/src/main/java/com/dazzilove/tobyspringstudy/user/dao/UserDaoSpringTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -32,9 +33,9 @@ public class UserDaoSpringTest {
 	public void setUp() {
 		dao = this.context.getBean("userDao", UserDao.class);
 
-		this.user1 = new User("ryush", "류성희1", "qlalfqjsgh");
+		this.user1 = new User("kimhun", "김훈1", "qlalfqjsgh");
 		this.user2 = new User("kimsh", "김성호1", "skfktkgkd");
-		this.user3 = new User("kimhun", "김훈1", "qlalfqjsgh");
+		this.user3 = new User("ryush", "류성희1", "qlalfqjsgh");
 	}
 	
 	@Test
@@ -77,6 +78,38 @@ public class UserDaoSpringTest {
 		assertThat(dao.getCount(), is(0));
 		
 		dao.get("unkonow_id");
+	}
+	
+	@Test
+	public void getAll() throws ClassNotFoundException, SQLException {
+		dao.deleteAll();
+		
+		List<User> users0 = dao.getAll();
+		assertThat(users0.size(), is(0));
+		
+		dao.add(user1);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+		
+		dao.add(user2);
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));
+		checkSameUser(user2, users2.get(1));
+		
+		dao.add(user3);
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user1, users3.get(0));
+		checkSameUser(user2, users3.get(1));
+		checkSameUser(user3, users3.get(2));
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
 
 }
